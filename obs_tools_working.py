5914d735e7a35b237a310351e4c1a1ad070dc9f8
fgueniat <contact@gueniat.fr>
2017-02-06 17:34:45 -0600

    $1

--- obs_tools_working.py
+++ obs_tools_working.py
@@ -6,9 +6,9 @@ import dyn_tools as dyn
 import plot_tools as pt
 from cantera_tools import chemreac
 
-measures_possible = ['cond', 'det','trace','max_svd']
+measures_possible = ['cond', 'det','trace']
 
-def slow_manifold(alpha_0, dx=None, f_def_obs=None,eps_ = None,time = None,rho = 1.e-3, measure = None, verbose = False,offset = 0.2,method = 'BFGS',isrecuit=True, niter=50,maxiter=[2000,250],n_recuit=50,r=None):
+def slow_manifold(alpha_0, dx=None, f_def_obs=None,eps_ = None,time = None,rho = 1.e-3, measure = None, verbose = False,offset = 0.2,method = 'BFGS',isrecuit=True, niter=50,maxiter=[2000,250],n_recuit=50):
 	'''Identify geometric constra ins based on observability analysis'''
 	if dx is None:
 		print 'run f_explore first'
@@ -22,7 +22,7 @@ def slow_manifold(alpha_0, dx=None, f_def_obs=None,eps_ = None,time = None,rho =
 	if f_def_obs is None:
 		print ' obserabl def as y=sum alpha_i x_i'
 		f_def_obs = lambda x,alpha: np.dot(alpha,x)
-	argums = (f_def_obs,rho,dx,eps_,time,False,False,False,measure,offset,verbose,r)
+	argums = (f_def_obs,rho,dx,eps_,time,False,False,False,measure,offset,verbose)
 	if type(maxiter) is not list: 
 		if type(maxiter) is int or type(maxiter) is float:
 			maxiter=[maxiter]
@@ -38,10 +38,10 @@ def slow_manifold(alpha_0, dx=None, f_def_obs=None,eps_ = None,time = None,rho =
 		alpha_0 = alpha_0.x
 	opt = {'maxiter':maxiter[0]}
 	#if no annealing, we finish with a regular minimization
-	a = solve(cost_gramian,alpha_0,method = method, tol = 1.e-5,args = argums,options=opt)
+	a = solve(cost_gramian,alpha_0,method = method, tol = 1.e-6,args = argums,options=opt)
 	return a
 
-def cost_gramian(alpha, f_def_obs=False, rho=0., dx=False, eps_=False, time=False, nt = False, ny=False, nx = False, measure=None, offset = 0.2, verbose = False,r=None):
+def cost_gramian(alpha, f_def_obs=False, rho=0., dx=False, eps_=False, time=False, nt = False, ny=False, nx = False, measure=None, offset = 0.2, verbose = False):
 	'''Compute the gramian and its  measure'''
 	# parameters
 	if f_def_obs is False:#def obs
@@ -52,10 +52,10 @@ def cost_gramian(alpha, f_def_obs=False, rho=0., dx=False, eps_=False, time=Fals
 	if ny is False:ny = np.size(fobs(dx[0,:,0,0]))
 	#
 	#computation of the gramian
-	w = EG(fobs = fobs, dx=dx, eps_ = eps_, time = time, nt = nt, nx = nx, ny = ny, offset = offset,r=r)
+	w = EG(fobs = fobs, dx=dx, eps_ = eps_, time = time, nt = nt, nx = nx, ny = ny, offset = offset)
 	if measure is None or measure not in measures_possible:
 		measure = 'trace'
-	norm_type = 1	
+	norm_type = 2	
 	if verbose is True:
 		print 'gramian'
 		print w
@@ -69,7 +69,7 @@ def cost_gramian(alpha, f_def_obs=False, rho=0., dx=False, eps_=False, time=Fals
 	return J
 
 
-def EG(fobs,dx,eps_,time,nt = False,nx = False,ny = False,offset = .2,r=None):
+def EG(fobs,dx,eps_,time,nt = False,nx = False,ny = False,offset = .2):
 	''' Empirical gramian'''
 	# parameters
 	if nt is False:nt = np.size(time)
@@ -93,13 +93,11 @@ def EG(fobs,dx,eps_,time,nt = False,nx = False,ny = False,offset = .2,r=None):
 	for it in xrange(nstart,nend):
 		dt = time[it]-time[it-1]
 		y=np.zeros((nx,2*ny))
-		if r is None:r0=np.zeros(nx)
-		else: r0 = r.h_z[it]
 		for ix in xrange(nx):# observation
 			if type(eps_) is float:epsi=2*eps_
 			else:epsi=2*eps_[ix]
-			y[ix,0:ny] = (fobs(dx[it,:,ix,0] - r0).flatten())/epsi
-			y[ix,ny:] = (fobs(dx[it,:,ix,1] - r0).flatten())/epsi
+			y[ix,0:ny] = fobs(dx[it,:,ix,0]).flatten()/epsi
+			y[ix,ny:] = fobs(dx[it,:,ix,1]).flatten()/epsi
 		# contribution of each observation to the gramian	
 		W = W+dt*np.dot(y,y.T) # equiv to W = W + y*y.T
 	#re normalization
@@ -117,13 +115,12 @@ def f_explore(fdyn,x0,time,eps_ = False):
 	nt = time.size
 	
 	dx = np.zeros((nt,nx,nx,2))
-	eps_effectif = np.zeros(nx)
 	if isinstance(fdyn,chemreac):#is fdyn a "cantera" object
 		# for each species/comp of the state space
 		for ix in xrange(nx):
 			#perturbation
 			base = np.zeros(nx)
-			if type(eps_) is float:base[ix] = eps_*x0[ix]
+			if type(eps_) is float:base[ix] = eps_
 			else:base[ix]=eps_[ix]
 			#def new reactions and integrate them
 			reaction_p = chemreac(cti=fdyn.cti,V=fdyn.V,problem = fdyn.problem)
@@ -139,8 +136,6 @@ def f_explore(fdyn,x0,time,eps_ = False):
 			# update the data
 			dx[:,:,ix,0] = xp
 			dx[:,:,ix,1] = xm
-			#eps_effectif[ix] = (reaction_p.h_z[0][ix] - reaction_m.h_z[0][ix])/reaction_p.h_z[0][ix] 
-			eps_effectif[ix] = reaction_p.h_z[0][ix] - reaction_m.h_z[0][ix] 
 	else:# then it is a dynamical system
 		for ix in xrange(nx):
 			base = np.zeros(nx)
@@ -149,7 +144,7 @@ def f_explore(fdyn,x0,time,eps_ = False):
 			xm = ode(fdyn,time,x0-base)
 			dx[:,:,ix,0] = xp
 			dx[:,:,ix,1] = xm
-	return dx,eps_effectif
+	return dx
 def f_explore_save(fdyn,x0,time,eps_ = False):
 	''' compute the perturbation around a given trajectory  defined by the initial conditions x0.
 	output has dimensions (nt x nx) x (nx x 2).
@@ -206,27 +201,26 @@ def p_explore(dd):
 	return xx,yy
 
 
-def M(W,measure='max_svd'):
+def M(W,measure='trace'):
 	''' Measure of the quality of the Gramian '''
 	if measure is 'cond':
 		J = np.linalg.cond(W)
 	if measure is 'det':
 		try:
-			J=np.linalg.det( np.linalg.pinv(W,rcond=1.e-2) )
+			J=np.linalg.det( np.linalg.inv(W) )
 		except:
 			J = 1000
 	if measure is 'trace':
 		try:
-			J=np.trace( np.linalg.pinv(W,rcond=1.e-2) )
+			J=np.trace( np.linalg.inv(W) )
 			if J<0:J=np.abs(J)
 		except:
 			J = 1000
 	if measure is 'mixte':J=M(W,'trace')+M(W,'det')
-	if measure is 'max_svd':
-		u,s,v = np.linalg.svd(W)
-		J=np.abs(np.log10(s[0])/np.log10(s[2]))
+
 	if J == np.inf:
 		J = 1.e18
+	
 	return np.log(J)
 
 def ode(fdyn,time,x0):
